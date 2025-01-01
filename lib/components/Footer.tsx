a01911905d0eae1423bc6a35a00c3333907094b9
Adam Simon <adamsimon85100@gmail.com>
2025-01-01 11:28:08 +0200

    contact anchor in Footer

--- lib/components/Footer.tsx
+++ lib/components/Footer.tsx
@@ -75,7 +75,7 @@ const propsDescriptor: ObjectDesc = {
             input: InputType.URL,
             default: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?auto=format&fit=crop&q=80",
         },
-        htmlId: { ...htmlIdDesc, default: "footer", },
+        htmlId: { ...htmlIdDesc, default: "contact", },
     }
 }
 

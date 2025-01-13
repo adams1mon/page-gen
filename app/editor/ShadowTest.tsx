1e2037b6ab7c744d516a242cf289e1b05d70f5d8
Adam Simon <adamsimon85100@gmail.com>
2025-01-13 20:45:06 +0200

    realize that contextmenu can't be used without a trigger

--- app/editor/ShadowTest.tsx
+++ app/editor/ShadowTest.tsx
@@ -4,6 +4,12 @@ import { ReactNode, useEffect, useRef, useState } from "react";
 import { ShadowEditorContainer } from "./ShadowEditorContainer";
 import { ComponentDivider } from "@/components/component-editor/component-input/ComponentDivider";
 import { ComponentContainer, findByIdInTree } from "@/lib/components-meta/ComponentContainer";
+import { ContextMenuContent, ContextMenuItem, ContextMenuSub, ContextMenuSubContent, ContextMenuSubTrigger } from "@radix-ui/react-context-menu";
+import { Copy, Edit, Plus, Trash2 } from "lucide-react";
+import { ComponentSelector } from "@/components/component-editor/component-input/ComponentSelector";
+import { Switch } from "@radix-ui/react-switch";
+import { useComponentClipboard } from "@/lib/store/component-clipboard-context";
+import { ContextMenu } from "@/components/ui/context-menu";
 export type CompFunc = (comp: ComponentDescriptor) => void;
 
 function createEditorContainers(
@@ -33,6 +39,7 @@ export function ShadowTest({ comp, onChange }: CompProps) {
     const ref = useRef(null);
 
     const [overlay, setOverlay] = useState(null);
+    const [contextMenu, setContextMenu] = useState(null);
 
     useEffect(() => {
 
@@ -65,14 +72,15 @@ export function ShadowTest({ comp, onChange }: CompProps) {
         const handleMouseOver = (e: Event) => {
             const target = e.target as HTMLElement;
             console.log("mouseover", target);
-            
+
             if (!target) return;
 
             const componentRoot = target.closest('[data-id]');
             if (componentRoot) {
 
-                console.log("target element desc", findByIdInTree(comp, componentRoot.dataset.id!));
-                
+                e.preventDefault();
+                e.stopPropagation();
+
                 const rect = componentRoot.getBoundingClientRect();
                 setOverlay({
                     id: componentRoot.dataset.id,
@@ -90,22 +98,39 @@ export function ShadowTest({ comp, onChange }: CompProps) {
             setOverlay(null);
         };
 
+        const handleContextMenu = (e: MouseEvent) => {
+            const target = e.target as HTMLElement;
+            if (!target) return;
+
+            const componentRoot = target.closest('[data-id]');
+            if (componentRoot) {
+
+                e.preventDefault();
+                e.stopPropagation();
+
+                const rect = componentRoot.getBoundingClientRect();
+                setContextMenu({
+                    id: componentRoot.dataset.id,
+                    position: { x: e.pageX, y: e.pageY },
+                    rect,
+                });
+            }
+        };
+
         shadow.addEventListener('mouseover', handleMouseOver);
         shadow.addEventListener('mouseout', handleMouseOut);
+        shadow.addEventListener('contextmenu', handleContextMenu);
 
-        //return () => {
-        //    shadow.removeEventListener('mouseover', handleMouseOver);
-        //    shadow.removeEventListener('mouseout', handleMouseOut);
-        //};
+        return () => {
+            shadow.removeEventListener('mouseover', handleMouseOver);
+            shadow.removeEventListener('mouseout', handleMouseOut);
+            shadow.removeEventListener('contextmenu', handleContextMenu);
+        };
 
     }, [comp, ref.current]);
 
     console.log("render shadowtest", comp);
 
-    if (overlay) {
-        console.log("overlay", overlay);
-    }
-        
 
     return (
         <>
@@ -142,6 +167,173 @@ export function ShadowTest({ comp, onChange }: CompProps) {
                 />
             )}
 
+            {contextMenu && (
+                <div
+                    style={{
+                        position: 'absolute',
+                        left: contextMenu.position.x + 'px',
+                        top: contextMenu.position.y + 'px',
+                        backgroundColor: 'white',
+                        border: '1px solid gray',
+                        zIndex: 1000,
+                    }}
+                >
+                    {
+                        //<ul>
+                        //    <li onClick={() => console.log("edit", contextMenu.id)}>Edit</li>
+                        //    <li onClick={() => console.log("delete", contextMenu.id)}>Delete</li>
+                        //</ul>
+
+                    }
+                    <MyContextMenu
+                        component={findByIdInTree(contextMenu.id)}
+                        overlayEnabled={false}
+                        overlayToggle={() => console.log("overlayToggle")}
+                        onEdit={() => console.log("edit")}
+                        onInsert={() => console.log("insert")}
+                        onRemove={() => console.log("remove")}
+                    />
+                </div>
+            )}
+
         </>
+    );
+}
+
+function MyContextMenu(
+    component,
+    overlayEnabled,
+    onOverlayToggle,
+    onEdit,
+    onInsert,
+    onRemove,
+) {
+
+    const { copy, paste, hasCopiedComponent } = useComponentClipboard();
+    //const [isOpen, setIsOpen] = useState(true);
+
+    return (
+        <ContextMenu>
+            <ContextMenuContent className="w-48">
+                <ContextMenuItem className="flex flex-col items-center mb-2">
+                    <p className="m-0">{component.name}</p>
+                    <hr className="w-full mt-2" />
+                </ContextMenuItem>
+
+                {/* Insert Above */}
+                <ContextMenuSub>
+                    <ContextMenuSubTrigger className="flex items-center">
+                        <Plus className="h-4 w-4 mr-2" />
+                        Insert Above
+                    </ContextMenuSubTrigger>
+                    <ContextMenuSubContent>
+                        <ComponentSelector onInsert={(comp) => onInsert(comp, 'before')}>
+                            <ContextMenuItem>
+                                Add Component
+                            </ContextMenuItem>
+                        </ComponentSelector>
+                        {
+                            //hasCopiedComponent() && (
+                            //<ContextMenuItem onClick={() => {
+                            //    const comp = paste();
+                            //    if (comp) onInsert(comp, 'before');
+                            //}}>
+                            //    <Clipboard className="h-4 w-4 mr-2" />
+                            //    Paste Component
+                            //</ContextMenuItem>
+                            //)
+                        }
+                    </ContextMenuSubContent>
+                </ContextMenuSub>
+
+                {/* Insert Below */}
+                <ContextMenuSub>
+                    <ContextMenuSubTrigger className="flex items-center">
+                        <Plus className="h-4 w-4 mr-2" />
+                        Insert Below
+                    </ContextMenuSubTrigger>
+                    <ContextMenuSubContent>
+                        <ComponentSelector onInsert={(comp) => onInsert(comp, 'after')}>
+                            <ContextMenuItem>
+                                Add Component
+                            </ContextMenuItem>
+                        </ComponentSelector>
+                        {
+                            //    hasCopiedComponent() && (
+                            //    <ContextMenuItem onClick={() => {
+                            //        const comp = paste();
+                            //        if (comp) onInsert(comp, 'after');
+                            //    }}>
+                            //        <Clipboard className="h-4 w-4 mr-2" />
+                            //        Paste Component
+                            //    </ContextMenuItem>
+                            //)
+                        }
+                    </ContextMenuSubContent>
+                </ContextMenuSub>
+
+                {/* Add Inside (if component accepts children) */}
+                {component.acceptsChildren && (
+                    <ContextMenuSub>
+                        <ContextMenuSubTrigger className="flex items-center">
+                            <Plus className="h-4 w-4 mr-2" />
+                            Add Inside
+                        </ContextMenuSubTrigger>
+                        <ContextMenuSubContent>
+                            <ComponentSelector onInsert={onInsert}>
+                                <ContextMenuItem>
+                                    Add Component
+                                </ContextMenuItem>
+                            </ComponentSelector>
+                            {
+                                //    hasCopiedComponent() && (
+                                //    <ContextMenuItem onClick={() => {
+                                //        const comp = paste();
+                                //        if (comp) onInsert(comp);
+                                //    }}>
+                                //        <Clipboard className="h-4 w-4 mr-2" />
+                                //        Paste Component
+                                //    </ContextMenuItem>
+                                //)
+                            }
+                        </ContextMenuSubContent>
+                    </ContextMenuSub>
+                )}
+
+                <ContextMenuItem onClick={onEdit} className="flex items-center">
+                    <Edit className="h-4 w-4 mr-2" />
+                    Edit properties
+                </ContextMenuItem>
+
+                <ContextMenuItem
+                    onClick={() => copy(component)}
+                    className="flex items-center"
+                >
+                    <Copy className="h-4 w-4 mr-2" />
+                    Copy component
+                </ContextMenuItem>
+
+                <ContextMenuItem
+                    className="flex items-center"
+                    onClick={onOverlayToggle}
+                >
+                    <Switch checked={overlayEnabled} className="transition-none" />
+                    Overlay enabled
+                </ContextMenuItem>
+
+                {onRemove && (
+                    <ContextMenuItem
+                        onClick={(e) => {
+                            e.preventDefault();
+                            onRemove(component);
+                        }}
+                        className="flex items-center text-destructive"
+                    >
+                        <Trash2 className="h-4 w-4 mr-2" />
+                        Delete component
+                    </ContextMenuItem>
+                )}
+            </ContextMenuContent>
+        </ContextMenu>
     );
 }
